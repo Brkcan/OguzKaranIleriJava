1acadef2f3e0439585b6788706f28ff26ee7e67e
Brkcan <brkcn006@gmail.com>
2021-03-15 01:29:55 +0300

    Reverse fonksiyonlar eklendi.../

--- src/main/java/org/csystem/App.java
+++ src/main/java/org/csystem/App.java
@@ -1,11 +1,12 @@
 package org.csystem;
 
-import org.csystem.util.NumberUtil;
+
+import org.csystem.util.Console;
 
 public class App {
-
     public static void main(String[] args) {
-
+        int a = Console.readInt("Bir ", "Hata");
+        Console.writeLine("A= " + a);
     }
 }
 
--- src/main/java/org/csystem/StringBuilderReverse/Util.java
+++ src/main/java/org/csystem/StringBuilderReverse/Util.java
@@ -9,9 +9,30 @@ public class Util {
         }
         return builder;
     }
+
+    public static String reverseString (String s1)
+    {
+        String s2 = "";
+
+        for (int i = s1.length() - 1; i >= 0; i--){
+            s2 += s1.charAt(i);
+        }
+        return s2;
+    }
+
+    public static StringBuilder reverseStringBuilder (String s1)
+    {
+        StringBuilder builder = new StringBuilder(s1);
+        builder.reverse();
+
+        return builder;
+    }
+
+
     public static void main(String[] args) {
         String s1 = "ankara";
-        //Util.reverse(s1);
         System.out.println(Util.reverse(s1));
+        System.out.println(Util.reverseString(s1));
+        System.out.println(Util.reverseStringBuilder(s1));
     }
 }
